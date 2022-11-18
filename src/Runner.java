6b8c08f8d41cb65968a1a519e3757214ef343b04
Sundar Nydhruva <nydhruva@Hanuman.local>
2022-11-18 15:58:39 -0800

    test commit

--- src/Runner.java
+++ src/Runner.java
@@ -3,21 +3,17 @@ import java.util.ArrayList;
 public class Runner {
     public static void main(String[] args) {
 
-        String[][] letterGrid = { { "n", "k", "c" }, { "a", "i", "w" }, { "r", "m", "o" }, { "d", "t", "v" } };
+        String[][] letterGrid = { { "r", "x", "b" }, { "o", "a", "g" }, { "d", "u", "m" }, { "e", "i", "l" } };
 
         LetterBoxed obj = new LetterBoxed("words.txt", letterGrid);
 
         ArrayList<String> res = obj.getValidWordsWithCross();
 
-        // for (int i = 0; i < res.size(); i++) {
-        // System.out.println(res.get(i));
-        // }
+        for (int i = 0; i < res.size(); i++) {
+            System.out.println(res.get(i));
+        }
 
-        // System.out.println(obj.getPangrams());
-
-        // System.out.println(obj.getWordsStartingWith(res, "a"));
-
-        System.out.println(obj.getWordsStartingWithAndContaining(res, "o", "k"));
+        System.out.println(obj.getWordsStartingWithAndContaining(res, "e", "a"));
 
     }
 }
