fe97be6f7f2339b933d5aeebeef504e8358ee602
Sundar Nydhruva <nydhruva@Hanuman.local>
2022-11-22 10:51:21 -0800

    added Test class

--- src/Runner.java
+++ src/Runner.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 public class Runner {
     public static void main(String[] args) {
-        String[][] letterGrid = { { "c", "s", "v" }, { "e", "p", "o" }, { "i", "f", "n" }, { "r", "a", "d" } };
+        String[][] letterGrid = { { "t", "h", "l" }, { "m", "i", "c" }, { "v", "r", "b" }, { "y", "o", "a" } };
 
         LetterBoxed obj = new LetterBoxed("words.txt", letterGrid);
         ArrayList<String> res = obj.getValidWordsWithCross();
@@ -10,7 +10,7 @@ public class Runner {
             System.out.println(res.get(i));
         }
 
-        System.out.println(obj.getWordsStartingWithAndContaining(res, "c", ""));
+        System.out.println(obj.getWordsStartingWithAndContaining(res, "i", "y"));
 
     }
 }
